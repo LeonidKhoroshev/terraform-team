0ff7fb7dad1b84600a6f7a636383d4bf2c15e846
Leonid Khoroshev <khoroshevlv@gmail.com>
2024-02-24 15:36:08 +0300

    24.02.24

--- variables.tf
+++ variables.tf
@@ -94,3 +94,27 @@ variable "subnets" {
                  { zone = "ru-central1-c", cidr = "10.0.3.0/24" }
                 ]
 }
+
+###variables for exercise 5
+
+variable "ip_address" {
+  type        = string
+  description = "ip address"
+  default     = "1920.1680.0.1"
+  validation {
+    condition = can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.ip_address))
+    error_message = "ip address is incorrect"
+  }
+}
+
+variable "ip_list" {
+  type        = list(string)
+  description = "list of ip addresses"
+  default     = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
+  validation {
+    condition = alltrue([
+      for a in var.ip_list: can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", a))
+    ])
+    error_message = "list of ip addresses is incorrect"
+  }
+}
